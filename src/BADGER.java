9ec06f0f55b807edc4ec7a6310e03f702767d304
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-12-06 19:31:11 -0800

    My ages & Badgers

---
+++ src/BADGER.java
@@ -0,0 +1,22 @@
+
+public class BADGER {
+	public static void main(String[] args) {
+	for(int j=0; j<3; j++) {	
+		for(int i=0; i<10; i++) {
+			System.out.print("Badger, ");}
+	
+		
+		System.out.println(); 
+		for(int i=0; i<2; i++) {
+		System.out.print("Mushroom, ");}
+		
+		System.out.println("a");
+		
+	}
+		
+		System.out.println("African! Snake, a snake\n" + "Snake! A snake, ooh it's a snake");
+		
+		
+		
+	}
+}
---
+++ src/MyAges.java
@@ -0,0 +1,7 @@
+
+public class MyAges {
+	public static void main(String[] args) {
+		for(int i=0; i<=13 ; i++);
+		
+	}
+}
